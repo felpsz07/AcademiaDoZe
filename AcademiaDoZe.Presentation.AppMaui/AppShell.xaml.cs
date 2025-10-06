e7349f57d39a404b56a8d27b38a3c6a5bd54e3fc
felip <felip@DESKTOP-3I3ISCC>
2025-10-05 22:34:41 -0300

    commit #2

--- AcademiaDoZe.Presentation.AppMaui/AppShell.xaml.cs
+++ AcademiaDoZe.Presentation.AppMaui/AppShell.xaml.cs
@@ -17,5 +17,6 @@ public partial class AppShell : Shell
 
     {
         Routing.RegisterRoute("logradouro", typeof(LogradouroPage));
+        Routing.RegisterRoute("colaborador", typeof(ColaboradorPage));
     }
 }
